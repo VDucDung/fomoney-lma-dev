b22381f5494e547bf083c468b1e62a004b89e04f
VDucDung <dunga3qv2@gmail.com>
2025-01-09 14:42:00 +0700

    config img

--- next.config.ts
+++ next.config.ts
@@ -19,7 +19,15 @@ const withPWA = require("next-pwa")({
 });
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "profile.line-scdn.net",
+        pathname: "/**",
+      },
+    ],
+  },
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
